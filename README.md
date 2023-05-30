54548438ca7ce8f14a333593ddbc1ff91c0f4bfa
eifr <eifr0980@gmail.com>
2023-05-30 18:21:45 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-vkmck1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-vkmck1)
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import RichTextEditor from 'react-rte';
+
+function App() {
+  return (
+    <>
+      <RichTextEditor />
+    </>
+  );
+}
+
+export default App;
